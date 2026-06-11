099c9c54406d8250ee6e410e10ab89783bc90ac3
Ainur Rozikin <rozikinainur742@gmail.com>
2026-06-11 19:03:58 +0700

    menambahkan komentar kedalam program

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
+// kode untuk menjalankan function 2 kali
 function duaKali (func) {
     func();
     func ();
 }
 
+// kode untuk mengenerate lemparan dadu 
 const lemparDadu = () => {
     let dadu = Math.floor(Math.random() * 6 + 1);
     console.log(dadu);
 }
 
+// callback function atau menjalankan fungsi di dalam fungsi
 console.log(duaKali(lemparDadu));
